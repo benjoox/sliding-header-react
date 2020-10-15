e964ea5f26211d3dec4152dc8392709d9c95812d
benyamin abedian <benyamin.abedian@gmail.com>
2020-10-15 14:16:58 +0200

    Updates

---
+++ precache-manifest.d888404213cce7fbf301543dedb0bbd0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378031127a7190b6f8d102eaacb309f5",
+    "url": "/sliding-header/index.html"
+  },
+  {
+    "revision": "1e67545ed622da398c9b",
+    "url": "/sliding-header/static/css/main.6cde11c4.chunk.css"
+  },
+  {
+    "revision": "b52db70f7c295ff406b1",
+    "url": "/sliding-header/static/js/2.70743d0b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sliding-header/static/js/2.70743d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e67545ed622da398c9b",
+    "url": "/sliding-header/static/js/main.6d35461a.chunk.js"
+  },
+  {
+    "revision": "c41b1217746ddf35cac5",
+    "url": "/sliding-header/static/js/runtime-main.52ac458c.js"
+  },
+  {
+    "revision": "05f794ccea17919ac87f016de4759b57",
+    "url": "/sliding-header/static/media/arrow-down.05f794cc.svg"
+  },
+  {
+    "revision": "17b6f2fe5c60b665396fc760b7f01bd4",
+    "url": "/sliding-header/static/media/banner-static-image.17b6f2fe.png"
+  },
+  {
+    "revision": "45affbbb11b9aab51d1d19f1ee3bf3ac",
+    "url": "/sliding-header/static/media/forum-dots.45affbbb.svg"
+  },
+  {
+    "revision": "0af58f48c97649f992eeb9dafd5663e7",
+    "url": "/sliding-header/static/media/forum-logo-text.0af58f48.svg"
+  },
+  {
+    "revision": "9ed42be7ab6645235675773c31943a37",
+    "url": "/sliding-header/static/media/profile.9ed42be7.svg"
+  },
+  {
+    "revision": "8fe28d91d773a12fd6e2bc1c55e68069",
+    "url": "/sliding-header/static/media/search.8fe28d91.svg"
+  }
+]);
